ec0b24b890b7570c874cc8a27c788bfe162a6897
Sebastian Guardo <sguardo@applaudostudios.dev>
2023-06-25 20:45:02 -0500

    Add cors to access the api from external domains

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 import fastapi as FastAPI
 from routes.products import product
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI.FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5500"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(product)
